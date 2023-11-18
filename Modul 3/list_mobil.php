c12dd5d7b98e9b22237ef666e0f713de35825c9d
Mahatma Rusyda Afif <90493429+Mahatma22@users.noreply.github.com>
2023-11-18 21:21:39 +0700

    Update list_mobil.php

--- Modul 3/list_mobil.php
+++ Modul 3/list_mobil.php
@@ -10,67 +10,51 @@
     <center>
         <div class="container">
             <h1>List Mobil</h1>
-                <table class = "table">
 
-                    <?php
-                    include("connect.php");
+            <?php
+            include("connect.php");
 
-                    // Buatlah query untuk mengambil data dari database (gunakan query SELECT)
-                        $query = mysqli_query($conn, "SELECT * FROM showroom_mobil");
+            // Buatlah query untuk mengambil data dari database (gunakan query SELECT)
 
-                        if ($query) {
-                            while($selects = mysqli_fetch_assoc($query)) {
-                        
-                        ?>
+            $sql = "SELECT * FROM showroom_mobil";
+            $result = $conn->query($sql);
 
-                        <tr>
-                            <th scope="row"><?= $selects['id']?></th>
-                            <td><?= $selects['nama_mobil']?></td>
-                            <td><?= $selects['brand_mobil']?></td>
-                            <td><?= $selects['warna_mobil']?></td>
-                            <td><?= $selects['tipe_mobil']?></td>
-                            <td><?= $selects['harga_mobil']?></td>
-                            <td>
-                                <a>
-                                <input type="button" value="Detail Mobil" href="form_detail_mobil">
-                                </a>
-                            </td>
-                        </tr>
-                    <?php
-                            }
-                        }
-                    ?>
-                    </table>
-
-<!--             // Buatlah perkondisian dimana: 
+            // Buatlah perkondisian dimana: 
             // 1. a. Apabila ada data dalam database, maka outputnya adalah semua data dalam database akan ditampilkan 
             //       (gunakan num_rows > 0, while, dan mysqli_fetch_assoc())
             //    b. Untuk setiap data yang ditampilkan, buatlah sebuah button atau link yang akan mengarahkan web ke halaman 
             //       form_detail_mobil.php dimana halaman tersebut akan menunjukkan seluruh data dari satu mobil berdasarkan id
-            // 2. Apabila tidak ada data dalam database, maka outputnya adalah pesan 'tidak ada data dalam tabel' -->
+            // 2. Apabila tidak ada data dalam database, maka outputnya adalah pesan 'tidak ada data dalam tabel'
 
-            <!--  **********************  1  **************************     -->
+            //<!--  **********************  1  **************************     -->
 
+            if ($result->num_rows > 0) {
+                echo "<table class='table table-striped'>";
+                echo "<thead><tr><th>ID</th><th>Nama Mobil</th><th>Brand Mobil</th><th>Warna Mobil</th><th>Tipe Mobil</th><th>Harga Mobil</th><th>Aksi</th></tr></thead><tbody>";
+
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr><td>" . $row['id']
+                    . "</td><td>" . $row['nama_mobil']
+                    . "</td><td>" . $row['brand_mobil']
+                    . "</td><td>" . $row['warna_mobil']
+                    . "</td><td>" . $row['tipe_mobil']
+                    . "</td><td>" . $row['harga_mobil']
+                    . "</td><td><a href='form_detail_mobil.php?id="
+                    . $row['id'] . "' class='btn btn-primary' id='detail'>Detail</a></td></tr>";
+                }
+                echo "</tbody></table>";
+
+            //<!--  **********************  1  **************************     -->
+
+            //<!--  **********************  2  **************************     -->
+
+            } else {
+                echo "Tidak ada data dalam tabel.";
+            }
+            $conn->close();
             
-            
-
-
-
-
-
-
-
-            <!--  **********************  1  **************************     -->
-
-            <!--  **********************  2  **************************     -->
-
-            
-            
-
-            
-            
-            <!--  **********************  2  **************************     -->
-            <!-- ?> -->
+            //<!--  **********************  2  **************************     -->
+            ?>
         </div>
     </center>
 </body>
